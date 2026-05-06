cf4a673a09f9e869dd48a68a8b112fe05367a100
Jorgon29 <00016823@uca.edu.sv>
2026-05-06 14:08:24 -0600

    :tada:
    Signed-off-by: Jorgon29 <00016823@uca.edu.sv>

---
+++ src/main/java/com/example/hogwartslaboratorio2ncapas/HogwartsLaboratorio2NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.example.hogwartslaboratorio2ncapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsLaboratorio2NCapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsLaboratorio2NCapasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hogwartslaboratorio2ncapas/HogwartsLaboratorio2NCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwartslaboratorio2ncapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsLaboratorio2NCapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
